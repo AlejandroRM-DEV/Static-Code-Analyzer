2d9db95387c5cba971833dc04052b1ef0ee8c3f2
Alejandro RM <alejandror45@gmail.com>
2023-07-26 00:49:31 -0600

    Stage 2/5

--- Static Code Analyzer/task/analyzer/code_analyzer.py
+++ Static Code Analyzer/task/analyzer/code_analyzer.py
@@ -1,14 +1,14 @@
-CODE_s001 = "S001 Too long"
-CODE_s002 = "S002 Indentation is not a multiple of four"
-CODE_s003 = "S003 Unnecessary semicolon after a statement (note that semicolons are acceptable in comments)"
-CODE_s004 = "S004 Less than two spaces before inline comments"
-CODE_s005 = "S005 TODO found (in comments only and case-insensitive)"
-CODE_s006 = "S006 More than two blank lines preceding"
+CODE_S001 = "S001 Too long"
+CODE_S002 = "S002 Indentation is not a multiple of four"
+CODE_S003 = "S003 Unnecessary semicolon after a statement (note that semicolons are acceptable in comments)"
+CODE_S004 = "S004 Less than two spaces before inline comments"
+CODE_S005 = "S005 TODO found (in comments only and case-insensitive)"
+CODE_S006 = "S006 More than two blank lines preceding"
 
 
 def test_s001(index, line):
     if len(line) > 79:
-        return f"Line {index}: {CODE_s001}"
+        return f"Line {index}: {CODE_S001}"
     else:
         return None
 
@@ -19,7 +19,7 @@ def test_s002(index, line):
 
     first_char = len(line) - len(line.lstrip())
     if first_char % 4 != 0:
-        return f"Line {index}: {CODE_s002}"
+        return f"Line {index}: {CODE_S002}"
     else:
         return None
 
@@ -29,7 +29,7 @@ def test_s003(index, line):
     statement = statement[0].split("'")  # Divide for possible string
     statement = statement[-1].split('"')  # Divide for possible string
     if statement[-1].find(";") > 0:
-        return f"Line {index}: {CODE_s003}"
+        return f"Line {index}: {CODE_S003}"
     else:
         return None
 
@@ -40,7 +40,7 @@ def test_s004(index, line):
 
     statement = line.split('#')
     if len(statement[0]) - len(statement[0].rstrip()) < 2:
-        return f"Line {index}: {CODE_s004}"
+        return f"Line {index}: {CODE_S004}"
     else:
         return None
 
@@ -51,7 +51,7 @@ def test_s005(index, line):
 
     statement = line.split('#')
     if "todo" in statement[1].lower():
-        return f"Line {index}: {CODE_s005}"
+        return f"Line {index}: {CODE_S005}"
     else:
         return None
 
@@ -70,7 +70,7 @@ if __name__ == "__main__":
             if len(line.strip()) == 0:
                 empty_lines = empty_lines + 1
             elif len(line.strip()) > 0 and empty_lines > 2:
-                errors.append(f"Line {index}: {CODE_s006}")
+                errors.append(f"Line {index}: {CODE_S006}")
                 empty_lines = 0
             else:
                 empty_lines = 0
