410bd776bbe14ee29b60b34a1a769ec0c1d9e118
Alejandro RM <alejandror45@gmail.com>
2023-07-26 11:49:45 -0600

    Stage 3/5: Analyze multi-file projects Learn how to work with multiple files and directories and process command-line arguments

--- Static Code Analyzer/task/analyzer/code_analyzer.py
+++ Static Code Analyzer/task/analyzer/code_analyzer.py
@@ -1,80 +1,103 @@
-CODE_S001 = "S001 Too long"
-CODE_S002 = "S002 Indentation is not a multiple of four"
-CODE_S003 = "S003 Unnecessary semicolon after a statement (note that semicolons are acceptable in comments)"
-CODE_S004 = "S004 Less than two spaces before inline comments"
-CODE_S005 = "S005 TODO found (in comments only and case-insensitive)"
-CODE_S006 = "S006 More than two blank lines preceding"
+from pathlib import Path
+import sys
 
 
-def test_s001(index, line):
-    if len(line) > 79:
-        return f"Line {index}: {CODE_S001}"
-    else:
-        return None
+class CodeAnalyzer:
+    CODE_S001 = "S001 Too long"
+    CODE_S002 = "S002 Indentation is not a multiple of four"
+    CODE_S003 = "S003 Unnecessary semicolon after a statement (note that semicolons are acceptable in comments)"
+    CODE_S004 = "S004 Less than two spaces before inline comments"
+    CODE_S005 = "S005 TODO found (in comments only and case-insensitive)"
+    CODE_S006 = "S006 More than two blank lines preceding"
 
+    def test_s001(self):
+        if len(self.line) > 79:
+            return f"{self.filename}: Line {self.line_num}: {self.CODE_S001}"
+        else:
+            return None
 
-def test_s002(index, line):
-    if len(line.strip()) == 0:
-        return None
+    def test_s002(self):
+        if len(self.line.strip()) == 0:
+            return None
 
-    first_char = len(line) - len(line.lstrip())
-    if first_char % 4 != 0:
-        return f"Line {index}: {CODE_S002}"
-    else:
-        return None
+        first_char = len(self.line) - len(self.line.lstrip())
+        if first_char % 4 != 0:
+            return f"{self.filename}: Line {self.line_num}: {self.CODE_S002}"
+        else:
+            return None
 
+    def test_s003(self):
+        statement = self.line.split('#')  # Divide statement and comment
+        statement = statement[0].split("'")  # Divide for possible string
+        statement = statement[-1].split('"')  # Divide for possible string
+        if statement[-1].find(";") > 0:
+            return f"{self.filename}: Line {self.line_num}: {self.CODE_S003}"
+        else:
+            return None
 
-def test_s003(index, line):
-    statement = line.split('#')  # Divide statement and comment
-    statement = statement[0].split("'")  # Divide for possible string
-    statement = statement[-1].split('"')  # Divide for possible string
-    if statement[-1].find(";") > 0:
-        return f"Line {index}: {CODE_S003}"
-    else:
-        return None
+    def test_s004(self):
+        if self.line.find("#") < 0 or self.line.startswith("#"):
+            return None
 
+        statement = self.line.split('#')
+        if len(statement[0]) - len(statement[0].rstrip()) < 2:
+            return f"{self.filename}: Line {self.line_num}: {self.CODE_S004}"
+        else:
+            return None
 
-def test_s004(index, line):
-    if line.find("#") < 0 or line.startswith("#"):
-        return None
+    def test_s005(self):
+        if self.line.find("#") < 0:
+            return None
 
-    statement = line.split('#')
-    if len(statement[0]) - len(statement[0].rstrip()) < 2:
-        return f"Line {index}: {CODE_S004}"
-    else:
-        return None
+        statement = self.line.split('#')
+        if "todo" in statement[1].lower():
+            return f"{self.filename}: Line {self.line_num}: {self.CODE_S005}"
+        else:
+            return None
 
+    def test_s006(self):
+        error = None
+        if len(self.line.strip()) == 0:
+            self.empty_lines = self.empty_lines + 1
+        elif len(self.line.strip()) > 0 and self.empty_lines > 2:
+            self.empty_lines = 0
+            error = f"{self.filename}: Line {self.line_num}: {self.CODE_S006}"
+        else:
+            self.empty_lines = 0
 
-def test_s005(index, line):
-    if line.find("#") < 0:
-        return None
+        return error
 
-    statement = line.split('#')
-    if "todo" in statement[1].lower():
-        return f"Line {index}: {CODE_S005}"
-    else:
-        return None
+    def run(self):
+        errors = []
+        with open(self.filename, "r") as file:
+            self.empty_lines = 0
+            for index, line in enumerate(file.readlines(), start=1):
+                self.line_num = index
+                self.line = line
+                errors.append(self.test_s001())
+                errors.append(self.test_s002())
+                errors.append(self.test_s003())
+                errors.append(self.test_s004())
+                errors.append(self.test_s005())
+                errors.append(self.test_s006())
+
+        for error in [error for error in errors if error is not None]:
+            print(error)
+
+    def __init__(self, filename):
+        self.empty_lines = None
+        self.line_num = None
+        self.line = None
+        self.filename = filename
 
 
 if __name__ == "__main__":
-    filename = input()
-    with open(filename, "r") as file:
-        empty_lines = 0
-        errors = []
-        for index, line in enumerate(file.readlines(), start=1):
-            errors.append(test_s001(index, line))
-            errors.append(test_s002(index, line))
-            errors.append(test_s003(index, line))
-            errors.append(test_s004(index, line))
-            errors.append(test_s005(index, line))
-            if len(line.strip()) == 0:
-                empty_lines = empty_lines + 1
-            elif len(line.strip()) > 0 and empty_lines > 2:
-                errors.append(f"Line {index}: {CODE_S006}")
-                empty_lines = 0
-            else:
-                empty_lines = 0
-
-        errors = [error for error in errors if error is not None]
-        for error in errors:
-            print(error)
+    args = sys.argv
+    for path in args[1:]:
+        if Path(path).is_file():
+            code_analyzer = CodeAnalyzer(path)
+            code_analyzer.run()
+        else:
+            for file in sorted([p for p in Path(path).iterdir()]):
+                code_analyzer = CodeAnalyzer(file)
+                code_analyzer.run()
---
+++ Topics/Copy of an object/Inspecting the deep copy/main.py
@@ -0,0 +1,5 @@
+import copy
+
+
+def solve(obj):
+    return type(obj) is not int and type(obj) is not float and type(obj) is not str
